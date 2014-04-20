5a1c8592f2fbdff98f07d8369fd313926e66671e
Michael Tolly <miketolly@gmail.com>
2014-04-20 18:23:58 -0500

    moved to libsndfile for wavstretch program
    works on large files!

--- wavstretch/Main.hs
+++ wavstretch/Main.hs
@@ -1,24 +1,15 @@
-module Main where
+module Main (main) where
 
 import Sound.RubberBand.Nice
 import Sound.RubberBand.Option (defaultOptions)
 
-import Data.WAVE
 import System.Environment (getArgs)
-import Control.Monad (forM_)
+import Control.Monad (replicateM_, when)
 import Data.List (transpose)
 
+import Sound.File.Sndfile
+import Sound.File.Sndfile.Buffer.Vector
 import qualified Data.Vector.Storable as V
-
-toArrays :: WAVESamples -> [V.Vector Float]
-toArrays ws = do
-  chan <- transpose ws
-  return $ V.fromList $ map (realToFrac . sampleToDouble) chan
-
-fromArrays :: [V.Vector Float] -> WAVESamples
-fromArrays ars = transpose $ do
-  ar <- ars
-  return $ map (doubleToSample . realToFrac) $ V.toList ar
 
 main :: IO ()
 main = do
@@ -26,56 +17,80 @@ main = do
   (fin, fout, ratio) <- case argv of
     [x, y, z] -> return (x, y, read z)
     _ -> error "invalid usage"
-  WAVE (WAVEHeader nchannels fps bits nframes) samples <- getWAVEFile fin
+  hndl <- openFile fin ReadMode defaultInfo
+  let Info nframes fps nchannels _fmt nsections skbl = hInfo hndl
+  when (not skbl) $ error "File is somehow not seekable?"
   s <- new (fromIntegral fps) (fromIntegral nchannels) defaultOptions ratio 1
   --setDebugLevel s 3
-  case nframes of
-    Just f -> setExpectedInputDuration s $ fromIntegral f
-    Nothing -> error "no number of frames"
+  setExpectedInputDuration s nframes
 
-  let blocks = groupsOf 1024 samples
+  let blockSize = 1024
+      (nonFinalBlocks, finalBlock) = case quotRem nframes blockSize of
+        (q, 0) -> (q - 1, blockSize)
+        (q, r) -> (q    , r        )
 
-  putStrLn "Supplying blocks..."
-  forM_ (zip [0..] blocks) $ \(i, block) -> do
-    study s (toArrays block) False
-    print (i :: Int)
-  study s (replicate nchannels V.empty) True
+  putStrLn $ "Supplying " ++ show (nonFinalBlocks + 1) ++ " blocks..."
+  replicateM_ nonFinalBlocks $ do
+    block <- getDeinterleaved hndl blockSize
+    study s block False
+  block <- getDeinterleaved hndl finalBlock
+  study s block True
   putStrLn "Done!"
 
-  WAVE _ samples' <- getWAVEFile fin
+  seekPos <- hSeek hndl AbsoluteSeek 0
+  when (seekPos /= 0) $ error "File somehow was not seeked to 0"
 
-  ws <- processRetrieveAll s samples'
-  let wav' = WAVE (WAVEHeader nchannels fps bits Nothing) ws
-  putWAVEFile fout wav'
+  hndl2 <- openFile fout WriteMode $ Info
+    { frames = floor $ fromIntegral nframes * ratio
+    , samplerate = fps
+    , channels = nchannels
+    , format = Format HeaderFormatWav SampleFormatPcm16 EndianFile
+    , sections = nsections
+    , seekable = True
+    }
+  processRetrieveAll 0 nframes s hndl hndl2
 
-processRetrieveAll :: State -> WAVESamples -> IO WAVESamples
-processRetrieveAll s samps = do
-  avail <- available s
-  case avail of
-    Nothing -> if null samps
-      then return []
-      else do
-        putStrLn "done processing but we have samples left"
-        return []
-    Just 0 -> do
-      req <- getSamplesRequired s
-      if req == 0
-        then do
-          putStrLn "thinking"
-          processRetrieveAll s samps
-        else case splitAt req samps of
-          ([]   , _   ) -> do
-            putStrLn "needs samples but we don't have any"
-            processRetrieveAll s samps
-          (block, rest) -> do
-            process s (toArrays block) $ null rest
-            processRetrieveAll s rest
-    Just n -> do
-      ars <- retrieve s n
-      rest <- processRetrieveAll s samps
-      return $ fromArrays ars ++ rest
+getDeinterleaved :: Handle -> Count -> IO [V.Vector Float]
+getDeinterleaved h c = do
+  let nchannels = channels $ hInfo h
+  buf <- hGetBuffer h c
+  case buf of
+    Nothing -> return $ replicate nchannels V.empty
+    Just b  -> return $ let
+      frameLists = groupsOf nchannels $ V.toList $ fromBuffer b
+      in [ V.fromList $ map (!! i) frameLists | i <- [0 .. nchannels - 1] ]
+
+interleave :: [V.Vector Float] -> V.Vector Float
+interleave = V.concat . map V.fromList . transpose . map V.toList
 
 groupsOf :: Int -> [a] -> [[a]]
 groupsOf n xs = case splitAt n xs of
   ([], []) -> []
   (ys, zs) -> ys : groupsOf n zs
+
+processRetrieveAll :: Count -> Count -> State -> Handle -> Handle -> IO ()
+processRetrieveAll pos len s hin hout = do
+  avail <- available s
+  case avail of
+    Nothing -> return ()
+    Just 0 -> do
+      req <- getSamplesRequired s
+      if req == 0
+        then do
+          putStrLn "Thinking"
+          processRetrieveAll pos len s hin hout
+        else do
+          let isFinal = len - pos <= req
+          if pos == len
+            then do
+              putStrLn "We don't have any samples to provide"
+              processRetrieveAll pos len s hin hout
+            else do
+              chans <- getDeinterleaved hin req
+              process s chans isFinal
+              let got = V.length $ head chans
+              processRetrieveAll (pos + got) len s hin hout
+    Just n -> do
+      bufs <- retrieve s n
+      _ <- hPutBuffer hout $ toBuffer $ interleave bufs
+      processRetrieveAll pos len s hin hout
